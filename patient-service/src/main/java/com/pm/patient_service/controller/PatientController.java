be2882aeaa72f586cee5a46328b3736680111a21
sandrohmt <sandrohmt@gmail.com>
2025-05-15 19:17:28 -0300

    feat: add PatientController

---
+++ patient-service/src/main/java/com/pm/patient_service/controller/PatientController.java
@@ -0,0 +1,25 @@
+package com.pm.patient_service.controller;
+
+import com.pm.patient_service.dto.PatientResponseDTO;
+import com.pm.patient_service.service.PatientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/patients")
+
+public class PatientController {
+    private final PatientService patientService;
+
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDTO>> getPatients() {
+        List<PatientResponseDTO> patients = patientService.getPatients();
+        return ResponseEntity.ok(patients);
+    }
+}
