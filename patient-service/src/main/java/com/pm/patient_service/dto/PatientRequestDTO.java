1e4c2663fef7b31df4a97afddeac96bbdf257464
Sandro <sandrohmt@gmail.com>
2025-05-16 16:59:29 -0300

    fix: apply registered date validation only on patient creation

--- patient-service/src/main/java/com/pm/patient_service/dto/PatientRequestDTO.java
+++ patient-service/src/main/java/com/pm/patient_service/dto/PatientRequestDTO.java
@@ -1,5 +1,6 @@
 package com.pm.patient_service.dto;
 
+import com.pm.patient_service.dto.validators.CreatePatientValidationGroup;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -24,6 +25,6 @@ public class PatientRequestDTO {
     @NotBlank(message = "Date of birth is required")
     private String dateOfBirth;
 
-    @NotNull(message = "Registered date is required")
+    @NotBlank(groups = CreatePatientValidationGroup.class, message = "Registered date is required")
     private String registeredDate;
 }
