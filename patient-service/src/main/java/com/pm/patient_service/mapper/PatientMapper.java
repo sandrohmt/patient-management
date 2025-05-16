ab8d96c55cec465577d385421b1d7c1204c4f64c
sandrohmt <sandrohmt@gmail.com>
2025-05-16 00:05:23 -0300

    feat: add method toModel in PatientMapper

--- patient-service/src/main/java/com/pm/patient_service/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/pm/patient_service/mapper/PatientMapper.java
@@ -1,7 +1,10 @@
 package com.pm.patient_service.mapper;
 
+import com.pm.patient_service.dto.PatientRequestDTO;
 import com.pm.patient_service.dto.PatientResponseDTO;
 import com.pm.patient_service.model.Patient;
+
+import java.time.LocalDate;
 
 public class PatientMapper {
 
@@ -13,5 +16,15 @@ public class PatientMapper {
         patientdto.setAddress(patient.getAddress());
         patientdto.setDateOfBirth(patient.getDateOfBirth().toString());
         return patientdto;
+    }
+
+    public static Patient toModel(PatientRequestDTO patientRequestDTO) {
+        Patient patient = new Patient();
+        patient.setName(patientRequestDTO.getName());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+        patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
+        return patient;
     }
 }
