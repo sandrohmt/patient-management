a8af3878fbb2a21cb004617f842a0c6125692110
Sandro <sandrohmt@gmail.com>
2025-05-30 16:03:05 -0300

    feat: add auth-service microservice

---
+++ auth-service/src/main/java/com/pm/authservice/model/User.java
@@ -0,0 +1,17 @@
+package com.pm.authservice.model;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+}
